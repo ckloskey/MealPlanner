9c658e14e31d63114c2e173b77c684f4dee0d277
ckloskey <ckloskey@gmail.com>
2018-06-20 15:29:58 -0500

    fixing something quick

--- MealPlanner/RapidApiConnection.cs
+++ MealPlanner/RapidApiConnection.cs
@@ -16,21 +16,17 @@ namespace MealPlanner
 
         public static void ConnectionString()
         {
-            //mashape (for unirest): JsDP2iHkAgmshwERFtVQk1t9uTlnp1bMLi4jsnW5wWkk97jreL
-            //rapidApi: 9524b29f-cbd6-4847-bbd8-2c54a2ddb144
-
             RapidAPI RapidApi = new RapidAPI(APIKeys.projectName, APIKeys.rapidKey);
         }
 
         public void GetRandomRecipes()
         {
             //Task<HttpResponse<MyClass>> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?number=1")
-            //.header("X-Mashape-Key", "JPy2T3JUc1msh6GJ4u64n95J0isip1r3yu0jsn1ITqrbPu6YqD")
+            //.header("X-Mashape-Key", APIKeys.mashapeKey)
             //.header("X-Mashape-Host", "spoonacular-recipe-food-nutrition-v1.p.mashape.com")
             //.asJson();
         }
 
-        //private static RapidAPI RapidApi = new RapidAPI(project: "ProjectName", key: "ProjectKey");
         private static RapidAPI RapidApi = new RapidAPI(APIKeys.projectName, key: APIKeys.rapidKey);
         //To call an API:
         public static void CallApi()
